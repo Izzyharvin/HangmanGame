c0e6c515f55374c67d30e2a587f22797b8adb335
Israel Harvin <izzy84.ih@gmail.com>
2019-07-11 18:19:08 -0400

    Made folders and files

---
+++ assests/javascript/gamefunction.js
@@ -0,0 +1,19 @@
+//The words that are going to be guessed
+//Created a variable where wordsAvaliable is egaul to the words in the array
+var wordsAvaliable = ["Batman", "Superman", "Flash", "Robin", "Spiderman", "Thor", "Black Widow", "Captain America", "Hulk"];
+
+//Variables for the game to function
+
+//Describtions on what to do in the game
+
+
+//Wins
+
+
+//Losses
+
+
+//Tries
+
+
+//Letters that are already guessed
